c14b9be882ba38da40d1ded5268da154e4b1f56d
MooMooYonders <181126455+MooMooYonders@users.noreply.github.com>
2025-09-20 21:11:35 +0800

    Add test cases for remarkcommand and addressbookparser

--- src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
+++ src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
@@ -1,4 +1,17 @@
 package seedu.address.logic.commands;
 
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+
 public class RemarkCommandTest {
+    private Model model = new ModelManager();
+
+    @Test
+    public void execute() {
+        assertCommandFailure(new RemarkCommand(), model, RemarkCommand.MESSAGE_NOT_IMPLEMENTED_YET);
+    }
 }
