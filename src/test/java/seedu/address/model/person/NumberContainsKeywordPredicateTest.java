96b0c77ed0368bd940cbbf8692d2be3355e09877
Nic_soh_lame <sohnicholas02@gmail.com>
2025-10-25 23:45:30 +0800

    Unit test for number and tag predicates

--- src/test/java/seedu/address/model/person/NumberContainsKeywordPredicateTest.java
+++ src/test/java/seedu/address/model/person/NumberContainsKeywordPredicateTest.java
@@ -1,5 +1,38 @@
 package seedu.address.model.person;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.testutil.PersonBuilder;
+
 public class NumberContainsKeywordPredicateTest {
+
+    @Test
+    public void test_numberContainsKeywords_returnTrue() {
+        //Exact Number
+        NumberContainsKeywordPredicate predicate = new NumberContainsKeywordPredicate(
+                "999");
+        assertTrue(predicate.test(new PersonBuilder().withPhone("999").build()));
+
+        //Substring Number
+        predicate = new NumberContainsKeywordPredicate(
+                "999");
+        assertTrue(predicate.test(new PersonBuilder().withPhone("9999999").build()));
+    }
+
+    @Test
+    public void test_numberDoesNotContainKeywords_returnsFalse() {
+        // Non-matching
+        NumberContainsKeywordPredicate predicate = new NumberContainsKeywordPredicate(
+                "999");
+        assertFalse(predicate.test(new PersonBuilder().withPhone("888").build()));
+    }
 }
 
--- src/test/java/seedu/address/model/person/TagContainsKeywordPredicateTest.java
+++ src/test/java/seedu/address/model/person/TagContainsKeywordPredicateTest.java
@@ -4,18 +4,43 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 import org.junit.jupiter.api.Test;
+import seedu.address.testutil.PersonBuilder;
 
 public class TagContainsKeywordPredicateTest {
 
     @Test
     public void test_TagContainsKeywords_returnsTrue() {
+        //One tag
         TagContainsKeywordPredicate predicate = new TagContainsKeywordPredicate(
                 Collections.singletonList("friend"));
+        assertTrue(predicate.test(new PersonBuilder().withTags("friend").build()));
 
+        //Multiple tags
+        predicate = new TagContainsKeywordPredicate(
+                Collections.singletonList("friend"));
+        assertTrue(predicate.test(new PersonBuilder().withTags("friend", "food", "buy").build()));
 
+        //Mixed Cases
+        predicate = new TagContainsKeywordPredicate(
+                Collections.singletonList("friend"));
+        assertTrue(predicate.test(new PersonBuilder().withTags("fRieNd").build()));
+    }
 
+    @Test
+    public void test_tagDoesNotContainKeywords_returnsFalse() {
+        // Non-matching keywords
+        TagContainsKeywordPredicate predicate = new TagContainsKeywordPredicate(
+                Collections.singletonList("friend")
+        );
+        assertFalse(predicate.test(new PersonBuilder().withTags("Alice").build()));
+
+        // Keywords match phone, email and address, but does not match name
+        predicate = new TagContainsKeywordPredicate(Arrays.asList("12345", "alice@email.com", "Main", "Street"));
+        assertFalse(predicate.test(new PersonBuilder().withTags("Alice").withPhone("12345")
+                .withEmail("alice@email.com").withAddress("Main Street").withTags("friend").build()));
     }
 }
