768f37fb1344819b0fe5a937699409fc24322870
Nic_soh_lame <sohnicholas02@gmail.com>
2025-10-27 18:00:59 +0800

    Add test cases for FindCommandParser to increase code coverage

--- src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
@@ -13,7 +13,9 @@ import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
+import seedu.address.model.person.NumberContainsKeywordPredicate;
 import seedu.address.model.person.Person;
+import seedu.address.model.person.TagContainsKeywordPredicate;
 
 public class FindCommandParserTest {
 
@@ -25,7 +27,24 @@ public class FindCommandParserTest {
     }
 
     @Test
-    public void parse_validArgs_returnsFindCommand() {
+    public void parse_emptyNameArg_throwsParseException() {
+        assertParseFailure(parser, "find n=", String.format(
+                MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_emptyNumberArg_throwsParseException() {
+        assertParseFailure(parser, "find mn=",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_emptyTagArg_throwsParseException() {
+        assertParseFailure(parser, "find t=", String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_validNameArgs_returnsFindCommand() {
         // no leading and trailing whitespaces
         List<Predicate<Person>> predicates = new ArrayList<>();
         predicates.add(new NameContainsKeywordsPredicate(Arrays.asList("Alice", "Bob")));
@@ -37,4 +56,27 @@ public class FindCommandParserTest {
         assertParseSuccess(parser, "find n= \n Alice \n Bob", expectedFindCommand);
     }
 
+    @Test
+    public void parse_validNumberArgs_returnsFindCommand() {
+        // no leading and trailing whitespaces
+        List<Predicate<Person>> predicates = new ArrayList<>();
+        predicates.add(new NumberContainsKeywordPredicate("999"));
+        FindCommand expectedFindCommand = new FindCommand(predicates);
+
+        assertParseSuccess(parser, "find mn=999", expectedFindCommand);
+    }
+
+    @Test
+    public void parse_validTagArgs_returnsFindCommand() {
+        // no leading and trailing whitespaces
+        List<Predicate<Person>> predicates = new ArrayList<>();
+        predicates.add(new TagContainsKeywordPredicate(Arrays.asList("Alice", "Bob")));
+        FindCommand expectedFindCommand = new FindCommand(predicates);
+
+        assertParseSuccess(parser, "find t=Alice Bob", expectedFindCommand);
+
+        // multiple whitespaces between keywords
+        assertParseSuccess(parser, "find t= Alice Bob", expectedFindCommand);
+
+    }
 }
