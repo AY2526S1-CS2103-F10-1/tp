ab7e5111f581feab3b68de7c63c317ae046f1819
MooMooYonders <181126455+MooMooYonders@users.noreply.github.com>
2025-10-28 22:16:20 +0800

    Implement SLAP in PersonCard

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -15,12 +15,14 @@ import seedu.address.MainApp;
 import seedu.address.model.person.Person;
 
 /**
- * An UI component that displays information of a {@code Person}.
+ * A UI component that displays information of a {@code Person}.
  */
 public class PersonCard extends UiPart<Region> {
 
     private static final String FXML = "PersonListCard.fxml";
+
     private static final String FLAG_IMAGE_PATH = "/images/flag.png";
+    private static final String FLAGGED_STYLE_CLASS = "flagged";
 
     /**
      * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
@@ -54,7 +56,7 @@ public class PersonCard extends UiPart<Region> {
     private Circle flag;
 
     /**
-     * Creates a {@code PersonCode} with the given {@code Person} and index to display.
+     * Creates a {@code PersonCard} with the given {@code Person} and index to display.
      */
     public PersonCard(Person person, int displayedIndex) {
         super(FXML);
@@ -67,16 +69,35 @@ public class PersonCard extends UiPart<Region> {
         address.setText(person.getAddress().value);
         email.setText(person.getEmail().value);
 
+        setTagsUI();
+        setMeetingsUI();
+
+        toggleFlagUI(person);
+    }
+
+    /**
+     * Populates the tags panel in the UI with the person's tags.
+     */
+    public void setTagsUI() {
         person.getTags().stream()
                 .sorted(Comparator.comparing(tag -> tag.tagName))
                 .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
+    }
 
+    /**
+     * Populates the meetings panel in the UI with the person's meetings.
+     * Each meeting is prefixed with its one-based index number.
+     */
+    public void setMeetingsUI() {
         IntStream.range(0, person.getMeetingCount())
-                .forEach(i -> meetings.getChildren()
-                        .add(new Label(String.format("%d. " + person.getMeetings().get(i).toString(),
-                                i + 1))));
+                .forEach(i -> {
+                    int oneBasedMeetingIndex = i + 1;
+                    String meetingText = person.getMeetings().get(i).toString();
+                    Label meetingLabel = new Label(String.format("%d. %s",
+                            oneBasedMeetingIndex, meetingText));
 
-        toggleFlagUI(person);
+                    meetings.getChildren().add(meetingLabel);
+                });
     }
 
     public HBox getCardPane() {
@@ -89,16 +110,22 @@ public class PersonCard extends UiPart<Region> {
 
     /**
      * Toggles the flag UI based on the person's flag status.
+     * If the person is flagged, shows the flag icon and applies
+     * the "flagged" CSS style class; otherwise, hides the icon
+     * and removes the style class.
+     *
+     * @param person The person whose flag status is used to update the UI.
      */
     public void toggleFlagUI(Person person) {
         if (person.isFlagged()) {
             Image image = new Image(MainApp.class.getResourceAsStream(FLAG_IMAGE_PATH));
             flag.setFill(new ImagePattern(image));
             flag.setVisible(true);
-            cardPane.getStyleClass().add("flagged");
+
+            cardPane.getStyleClass().add(FLAGGED_STYLE_CLASS);
         } else {
             flag.setVisible(false);
-            cardPane.getStyleClass().removeAll("flagged");
+            cardPane.getStyleClass().removeAll(FLAGGED_STYLE_CLASS);
         }
     }
 
